890dbe7a949a16561cdc855a03c17e4061383196
Francisco Gutierrez <freefrancisco@gmail.com>
2018-12-06 19:45:34 -0800

    added forward and backward general rnn types plus tests to print what they are doing

--- src/NNTypes.hs
+++ src/NNTypes.hs
@@ -1,1 +1,24 @@
 module NNTypes where
+
+import Data.List (unfoldr, mapAccumR, mapAccumL)
+
+
+encodingRnn :: (s -> x -> s) -> s -> [x] -> s
+encodingRnn a s = foldl a s
+
+generatingRnn :: (s -> Maybe (y, s)) -> s -> [y]
+generatingRnn a s = unfoldr a s
+
+generalRnn :: (s -> x -> (s, y)) -> s -> [x] -> (s, [y])
+generalRnn a s = mapAccumR a s
+
+-- reverse rnn
+generalRnn' :: (s -> x -> (s, y)) -> s -> [x] -> (s, [y])
+generalRnn' a s = mapAccumL a s
+
+bidirectionalRnn :: (s -> x -> (s, y)) -> s -> -- forward rnn
+                    (s -> x -> (s, y)) -> s -> -- reverse rnn
+                    [x] -> ((s, s), [(y,y)])   -- totals and zipped sequences
+bidirectionalRnn a s a' s' xs = ((final, final'), zip ys ys') where
+    (final,  ys)  = generalRnn  a  s  xs
+    (final', ys') = generalRnn' a' s' xs
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,40 @@
 import Test.Hspec
 import Test.QuickCheck
 
+import NNTypes
+
+xs = ["a", "b", "c", "d"]
+s0 = "S0"
+
+-- generalLayer :: [Int] -> Int -> ([Int], String)
+generalLayer s x = (s ++ x, "x: " ++ x ++ ", s: " ++ s)
+
+printResults title (final, ys) = do
+    putStrLn title
+    putStrLn "final State: "
+    print final
+    putStrLn "ys: "
+    print ys
+
 main :: IO ()
 main = hspec $ do
-    describe "tests go here" $ do
-        it "does something" $ do
-            putStrLn "Test suite not yet implemented"
+    describe "encodingRnn" $ do
+        it "runs and produces a final state" $ do
+            let layer s x = sf where (sf, y) = generalLayer s x
+            let final = encodingRnn layer s0 xs
+            printResults "Encoding RNN" (final, ["Not applicable"])
+
+    describe "generalRnn" $ do
+        it "runs forward" $ do
+            let (final, ys) = generalRnn generalLayer s0 xs
+            printResults "Forward RNN" (final, ys)
+
+    describe "generalRnn'" $ do
+        it "runs backwards" $ do
+            let (final, ys) = generalRnn' generalLayer s0 xs
+            printResults "Backwards RNN" (final, ys)
+
+    describe "bidirectionalRnn" $ do
+        it "zips forward and backward runs" $ do
+            let (finals, ys) = bidirectionalRnn generalLayer s0 generalLayer s0 xs
+            printResults "Bidirectional RNN" (finals, ys)
